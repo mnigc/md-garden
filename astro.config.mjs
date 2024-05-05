e477bf6a52214692250bf738be307047f3373fa0
郭超 <guochao6040@live.com>
2024-05-05 22:01:40 +0800

    调整样式，博客md文件转移

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import mdx from "@astrojs/mdx";
-mdx()
+import mdx from "@astrojs/mdx";mdx()
+import starlightImageZoom from 'starlight-image-zoom';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
-    title: 'Digital Garden',
+    title: '不知名角落',
     favicon: './src/assets/my-logo.svg',
+    plugins: [starlightImageZoom()],
     social: {
       github: 'https://github.com/mnigc'
     },
@@ -22,10 +23,18 @@ export default defineConfig({
         lang: 'zh-CN'
       }
     },
+
+    lastUpdated: true, // 开启最近更新时间
+
+    pagination: false, // 关闭分页功能
+
+    tableOfContents: {
+      minHeadingLevel: 2, maxHeadingLevel: 6,
+    },
+
     customCss: [
     // 自定义 CSS 文件的相对路径
-    './src/styles/customCss.css']
+    './src/styles/customCss.css']  
   }), 
-  
  ]
 });
