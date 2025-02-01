3ecce8a5cd7d6791d8180b952159614a7c2a2109
郭超 <guochao6040@live.com>
2025-02-01 14:13:27 +0800

    update astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,15 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection,z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
+
+// 定义一个名为 "docs" 的集合
+const docsCollection = defineCollection({
+	schema: z.object({
+	  title: z.string(), // 每篇文章必须有 title 字段
+	  description: z.string().optional(), // 可选字段
+	  date: z.date().optional(), // 可选字段
+	}),
+  });
+  
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
